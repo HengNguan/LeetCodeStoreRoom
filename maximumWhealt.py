065a5d7af12f49b13ceead4e72001114d50ddef2
Ting Heng Nguan <68612613+HengNguan@users.noreply.github.com>
2025-12-21 11:59:25 +0000

    Remove obsolete solution files for FizzBuzz, Sum 1D Array, add2int, Maximum Wealth, and Steps to Zero

--- maximumWhealt.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        customer_max = 0
-        for account in accounts:
-            customer_sum = sum(account)
-            customer_max = max(customer_max, customer_sum)
-        return customer_max 
-
